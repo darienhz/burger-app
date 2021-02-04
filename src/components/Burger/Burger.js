c6f524a24f1ca89195d125a1bf699186d9501a53
Darien Hernandez <darienhz89@gmail.com>
2021-02-03 16:33:54 -0800

    mapped through ingredients object to return an array and used reduced to get one single array

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey}/>
+      });
+    })
+      .reduce((arr, el) => {
+        return arr.concat(el)
+      }, []);
+      console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+        {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
   );
