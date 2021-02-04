467f5bf2dcf0be72379219fbd3349e3c17f19aee
Darien Hernandez <darienhz89@gmail.com>
2021-02-03 16:37:01 -0800

    added a p to tell user to add ingredients if none are present

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,7 +3,7 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey}/>
@@ -12,6 +12,9 @@ const burger = (props) => {
       .reduce((arr, el) => {
         return arr.concat(el)
       }, []);
+    if (transformedIngredients.length === 0) {
+      transformedIngredients = <p>Please start adding ingredients</p>
+    }
       console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
