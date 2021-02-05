25e3f06e060a907ac6013df0c6dc4eeb64e21b8e
Darien Hernandez <darienhz89@gmail.com>
2021-02-04 19:11:46 -0800

    added a backdrop for when the modal pops up

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import classes from './Backdrop.css'
 
 const backdrop = (props) => (
-  props.show ? <div></div> : null
+  props.show ? <div className={classes.backdrop}></div> : null
 );
 
 export default backdrop;
